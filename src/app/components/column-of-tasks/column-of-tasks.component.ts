d1717336f78fc214f564ea1a5e098c11841ea2d6
gerwasiy <gerwasiy@gmail.com>
2022-10-14 01:26:38 +0300

    realization drag&drop

--- src/app/components/column-of-tasks/column-of-tasks.component.ts
+++ src/app/components/column-of-tasks/column-of-tasks.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import {faCircle} from '@fortawesome/free-solid-svg-icons';
-import {faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
-import {faCircleHalfStroke} from '@fortawesome/free-solid-svg-icons';
-
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 import { columnOfTasks } from 'src/app/models/column-of-tasks';
-import { taskCard } from 'src/app/models/task-card';
+import { TaskCardModel } from 'src/app/models/task-card';
 
 @Component({
   selector: 'app-column-of-tasks',
@@ -16,20 +19,19 @@ import { taskCard } from 'src/app/models/task-card';
 })
 export class ColumnOfTasks implements OnInit {
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<TaskCardModel[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
@@ -37,55 +39,48 @@ export class ColumnOfTasks implements OnInit {
   addTaskCard = faPlus;
 
   @Input() columnOfTasks?: columnOfTasks;
-  
-  progressOfTasks = { 
-    toDo:faCircleExclamation,
-    inProgress:faCircleHalfStroke,
-    ready:faCircle
-  }
-  
+
+  progressOfTasks = {
+    toDo: faCircleExclamation,
+    inProgress: faCircleHalfStroke,
+    ready: faCircle,
+  };
+
   columnsOfTasks: columnOfTasks[] = [
-    { 
+    {
       icon: this.progressOfTasks.toDo,
-      title:'To do'
+      title: 'To do',
     },
     {
       icon: this.progressOfTasks.inProgress,
-      title:'In progress'
+      title: 'In progress',
     },
     {
       icon: this.progressOfTasks.ready,
-      title:'Ready'
-    }
-  ]
-  
-  taskCardsToDo: taskCard[] = [ ];
-  taskCardsInProgress: taskCard[] = [ ];
-  taskCardsInReady: taskCard[] = [ ];
+      title: 'Ready',
+    },
+  ];
+
+  taskCardsToDo: TaskCardModel[] = [];
+  taskCardsInProgress: TaskCardModel[] = [];
+  taskCardsInReady: TaskCardModel[] = [];
+
+  taskCardTemplate:TaskCardModel = {
+    title: 'Issue Title',
+    description: 'description',
+    creationData: new Date(),
+  }
 
   createTaskCardInToDo() {
-    this.taskCardsToDo.push({
-      title: 'Issue Title',
-      description: 'description',
-      creationData: new Date()
-    });
+    this.taskCardsToDo.push(this.taskCardTemplate);
   }
   createTaskCardInProgress() {
-    this.taskCardsInProgress.push({
-      title: 'Issue Title',
-      description: 'description',
-      creationData: new Date()
-    });
+    this.taskCardsInProgress.push(this.taskCardTemplate);
   }
   createTaskCardInReady() {
-    this.taskCardsInReady.push({
-      title: 'Issue Title',
-      description: 'description',
-      creationData: new Date()
-    });
+    this.taskCardsInReady.push(this.taskCardTemplate);
   }
 
-  
   constructor() {}
 
   ngOnInit(): void {}
