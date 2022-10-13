cf9f3658f431a6c9f60935bfa9e6fba02071e4e9
gerwasiy <gerwasiy@gmail.com>
2022-10-13 21:19:21 +0300

    testing

--- src/app/components/column-of-tasks/column-of-tasks.component.ts
+++ src/app/components/column-of-tasks/column-of-tasks.component.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { columnOfTasks } from 'src/app/models/column-of-tasks';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import {faCircle} from '@fortawesome/free-solid-svg-icons';
+import {faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
+import {faCircleHalfStroke} from '@fortawesome/free-solid-svg-icons';
+
+
+import { columnOfTasks } from 'src/app/models/column-of-tasks';
 import { taskCard } from 'src/app/models/task-card';
 
 @Component({
@@ -10,23 +16,76 @@ import { taskCard } from 'src/app/models/task-card';
 })
 export class ColumnOfTasks implements OnInit {
 
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
   addTaskCard = faPlus;
 
   @Input() columnOfTasks?: columnOfTasks;
   
+  progressOfTasks = { 
+    toDo:faCircleExclamation,
+    inProgress:faCircleHalfStroke,
+    ready:faCircle
+  }
   
+  columnsOfTasks: columnOfTasks[] = [
+    { 
+      icon: this.progressOfTasks.toDo,
+      title:'To do'
+    },
+    {
+      icon: this.progressOfTasks.inProgress,
+      title:'In progress'
+    },
+    {
+      icon: this.progressOfTasks.ready,
+      title:'Ready'
+    }
+  ]
   
-  taskCards: taskCard[] = [ ];
-  createTaskCard() {
-    this.taskCards.push({
+  taskCardsToDo: taskCard[] = [ ];
+  taskCardsInProgress: taskCard[] = [ ];
+  taskCardsInReady: taskCard[] = [ ];
+
+  createTaskCardInToDo() {
+    this.taskCardsToDo.push({
       title: 'Issue Title',
       description: 'description',
       creationData: new Date()
     });
-    
-   
+  }
+  createTaskCardInProgress() {
+    this.taskCardsInProgress.push({
+      title: 'Issue Title',
+      description: 'description',
+      creationData: new Date()
+    });
+  }
+  createTaskCardInReady() {
+    this.taskCardsInReady.push({
+      title: 'Issue Title',
+      description: 'description',
+      creationData: new Date()
+    });
   }
 
+  
   constructor() {}
 
   ngOnInit(): void {}
